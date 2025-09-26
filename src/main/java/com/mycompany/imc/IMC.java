5d51f5ddb7230e6b88197ee3bc23f21c09d73079
balriobdb <mauriciofreitasb3@gmail.com>
2025-09-26 19:45:57 -0300

    Inicializando Reopositório
    Incialização do repositório da calculadora de IMC

---
+++ src/main/java/com/mycompany/imc/IMC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.imc;
+
+/**
+ *
+ * @author mauricio.freitas1
+ */
+public class IMC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
